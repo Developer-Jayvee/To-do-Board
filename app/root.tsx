fd061818de6922d951a4359d25065866c674f21f
Jayvee <jayvee.github0703@gmail.com>
2026-03-27 09:41:59 +0800

    To do Board first commit

--- app/root.tsx
+++ app/root.tsx
@@ -10,18 +10,13 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "To Do Board" },
+    { name: "description", content: "" },
+  ];
+}
+
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
