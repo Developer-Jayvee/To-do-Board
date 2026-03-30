6aba6c2fd9abe0f8b8a7edaee424910ead7d130c
Jayvee <jayvee.github0703@gmail.com>
2026-03-30 12:47:09 +0800

    UI:Register Page

---
+++ components/ui/Button.tsx
@@ -0,0 +1,10 @@
+
+import  { type ButtonHTMLAttributes } from "react"
+
+export default function Button({ children, ...rest }: ButtonHTMLAttributes<HTMLButtonElement>) {
+    return (
+        <button {...rest} className={`btn-primary custom ${rest.className}`}>
+            {children}
+        </button>
+    )
+}
