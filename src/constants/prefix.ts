813f311bb283cfeacec8813cb2090fddaf074fc3
Jayvee <jayvee.github0703@gmail.com>
2026-04-09 22:03:47 +0800

    Added notif and email sending

--- src/constants/prefix.ts
+++ src/constants/prefix.ts
@@ -4,3 +4,4 @@ export const PREFIX_TICKET = "ticket"
 export const PREFIX_CATEGORY = "categories"
 export const PREFIX_LABEL = "labels"
 export const PREFIX_PROGRESS = "progress";
+export const PREFIX_NOTIF = "notif"
